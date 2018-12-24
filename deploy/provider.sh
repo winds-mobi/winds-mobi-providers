ab280c603008b8b4a84816abf755084fdf03efdd
Yann Savary <yann.savary@gmail.com>
2018-12-24 12:28:21 +0100

    Updated deployment files

---
+++ deploy/provider.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Makes sure we exit if flock fails.
+set -e
+
+PROVIDER=$1
+(
+  flock -n 200 || exit 1
+
+  /home/windmobile/.pyenv/versions/providers/bin/python /home/windmobile/winds-mobi-providers/$PROVIDER.py 1>/dev/null 2>>/var/log/windmobile/$PROVIDER.err
+
+) 200>/home/windmobile/winds-mobi-providers/$PROVIDER.lock
