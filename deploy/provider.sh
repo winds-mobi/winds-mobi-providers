b29cf80547eb1a584c1556a3f6d1c7d9e7c512ae
Yann Savary <yann.savary@gmail.com>
2019-05-19 21:51:27 +0200

    Fixed mixed 2.7 - 3.6 python environment

--- deploy/provider.sh
+++ deploy/provider.sh
@@ -7,6 +7,6 @@ PROVIDER=$1
 (
   flock -n 200 || exit 1
 
-  /home/windmobile/.virtualenvs/winds-mobi-providers/bin/python /home/windmobile/winds-mobi-providers/$PROVIDER.py 1>/dev/null 2>>/data/var/log/winds.mobi/$PROVIDER.err
+  /home/windmobile/.local/share/virtualenvs/winds-mobi-providers-SLXTQqYQ/bin/python /home/windmobile/winds-mobi-providers/$PROVIDER.py 1>/dev/null 2>>/data/var/log/winds.mobi/$PROVIDER.err
 
 ) 200>/home/windmobile/winds-mobi-providers/$PROVIDER.lock
