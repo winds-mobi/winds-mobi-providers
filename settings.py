8ad120e623ea921c3f1a77c065fc4351a717c9b9
Yann Savary <ysavary@users.noreply.github.com>
2019-08-30 11:48:06 +0200

    Add documentation to run providers

--- settings.py
+++ settings.py
@@ -1,22 +1,30 @@
+# COMMONS
 MONGODB_URL = 'mongodb://localhost:27017/winds_mobi'
 REDIS_URL = 'redis://localhost:6379/0'
+GOOGLE_API_KEY = ''  # Mandatory to run any provider
 
+# Logging and monitoring
 LOG_DIR = None
 SENTRY_URL = ''
 ENVIRONMENT = 'development'
 
-GOOGLE_API_KEY = ''
-CHECKWX_API_KEY = ''
-ROMMA_KEY = ''
-
-WINDLINE_SQL_URL = ''
-
+# PROVIDERS
+# JDC
 JDC_IMAP_SERVER = ''
 JDC_IMAP_USERNAME = ''
 JDC_IMAP_PASSWORD = ''
 JDC_DELETE_EMAILS = False
 JDC_PHP_PATH = 'php'
 JDC_ADMIN_DB_URL = 'postgres://postgres:postgres@localhost:5432/winds_mobi'
+
+# Windline
+WINDLINE_SQL_URL = ''
+
+# METAR
+CHECKWX_API_KEY = ''
+
+# Romma
+ROMMA_KEY = ''
 
 try:
     from local_settings import *  # noqa
