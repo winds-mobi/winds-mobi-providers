ffae62d251b926da6d48c707ec6146d675fc2221
pararnold <64554629+pararnold@users.noreply.github.com>
2022-03-08 13:45:11 +0100

    Added station Grimsel
    Added station Grimsel

--- providers/fluggruppe_aletsch.py
+++ providers/fluggruppe_aletsch.py
@@ -24,6 +24,7 @@ class FluggruppeAletsch(Provider):
         ["fieschertal", "fiesch/XML"],
         ["chaeserstatt", "chaeserstatt/XML"],
         ["jeizinen", "jeizinen/XML"],
+        ["grimsel", "grimselpass/XML"],
     ]
 
     def process_data(self):
