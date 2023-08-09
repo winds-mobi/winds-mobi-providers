5b7c2681599f764ed18c58ddd57de97bae023342
Yann Savary <yann@winds.mobi>
2023-08-09 21:30:33 +0200

    Upgrade FFVL provider with the new API. Waiting for encoding fix on their side...

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-# COMMONS
+# Commons
 MONGODB_URL = os.environ.get("MONGODB_URL") or "mongodb://localhost:27017/winds_mobi"
 REDIS_URL = os.environ.get("REDIS_URL") or "redis://localhost:6379/0"
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
@@ -9,24 +9,13 @@ GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 SENTRY_URL = os.environ.get("SENTRY_URL")
 ENVIRONMENT = os.environ.get("ENVIRONMENT") or "development"
 
-# PROVIDERS
+# Providers
 ADMIN_DB_URL = os.environ.get("ADMIN_DB_URL") or "postgres://postgres:postgres@localhost:5432/winds_mobi"
-
-# Windline
-WINDLINE_SQL_URL = os.environ.get("WINDLINE_SQL_URL")
-
-# METAR
-CHECKWX_API_KEY = os.environ.get("CHECKWX_API_KEY")
-
-# Romma
-ROMMA_KEY = os.environ.get("ROMMA_KEY")
-
-# iWeathar
-IWEATHAR_KEY = os.environ.get("IWEATHAR_KEY")
-
-# BornToFly
 BORN_TO_FLY_VENDOR_ID = os.environ.get("BORN_TO_FLY_VENDOR_ID")
 BORN_TO_FLY_DEVICE_ID = os.environ.get("BORN_TO_FLY_DEVICE_ID")
-
-# Windy
+CHECKWX_API_KEY = os.environ.get("CHECKWX_API_KEY")  # For METAR provider
+FFVL_API_KEY = os.environ.get("FFVL_API_KEY")
+IWEATHAR_KEY = os.environ.get("IWEATHAR_KEY")
+ROMMA_KEY = os.environ.get("ROMMA_KEY")
+WINDLINE_SQL_URL = os.environ.get("WINDLINE_SQL_URL")
 WINDY_API_KEY = os.environ.get("WINDY_API_KEY")
